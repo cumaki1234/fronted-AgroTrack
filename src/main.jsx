ad9a878bbf9a3706a699f6552c22fdea7ea17796
cumaki1234 <eduardochuez_2003@outlook.com>
2026-02-28 05:51:56 -0500

    asasaaaaa

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext"
+import { HuertoProvider } from "./context/HuertoContext"
 import './index.css'
-import 'antd/dist/reset.css';
+import 'antd/dist/reset.css'
 import App from './App'
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <HuertoProvider>
+            <App />
+          </HuertoProvider>
+        </AuthProvider>
+      </BrowserRouter>
     </StrictMode>
-  );
+  )
 }
